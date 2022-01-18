54979bbd8c1604267a835bb7544fcce871b97b1f
Sea Viseth <seaviseth@gmail.com>
2022-01-18 16:22:44 +0700

    image upload;

--- index.js
+++ index.js
@@ -9,21 +9,19 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://192.168.1.8:3000"],
+    origin: ["http://localhost:4000", "http://192.168.1.8:4000"],
     credentials: true,
   })
 );
-app.use("/public/", express.static(path.join(__dirname, "public/video")));
+app.use("/public/", express.static(path.join(__dirname, "public/")));
 
 // ===== check if public folder exist ====
-if (fs.existsSync("./public/video")) {
-  console.log("exist");
-} else {
+if (!fs.existsSync("./public")) {
   fs.mkdirSync("./public/video", { recursive: true });
+  fs.mkdirSync("./public/image", { recursive: true });
 }
-
 // ===== set file path/name =====
-const fileStorageEngine = multer.diskStorage({
+const fileStorageEngineVideo = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./public/video");
   },
@@ -32,31 +30,51 @@ const fileStorageEngine = multer.diskStorage({
   },
 });
 
+const fileStorageEngineImage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/image");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "--" + file.originalname);
+  },
+});
+
 //==== middleware ====
-const upload = multer({ storage: fileStorageEngine });
+const uploadVideo = multer({ storage: fileStorageEngineVideo });
+const uploadImage = multer({ storage: fileStorageEngineImage });
 
 app.get("/", (_, res) => {
   res.sendFile(path.join(__dirname, "home.html"));
 });
 
-// ==== upload video ====
-app.post("/upload-video", upload.single("video"), (req, res) => {
-  res.json({ data: req.file.filename });
-});
-
-app.post("/upload-videos", upload.array("videos"), (req, res) => {
+// === upload images ===
+app.post("/upload-images", uploadImage.array("images"), (req, res) => {
   res.json({ data: req.files[0].filename });
 });
 
-// ==== delete video ====
-app.post("/delete", (req, res) => {
-  let videoPath = path.join(__dirname, "/public/video/" + req.body.video);
-  if (fs.existsSync(videoPath)) {
-    fs.unlinkSync(videoPath);
-    ``;
-  }
-  res.send("Video Deleted!");
+// ==== upload video ====
+app.post("/upload-videos", uploadVideo.array("videos"), (req, res) => {
+  res.json({ data: req.files[0].filename });
 });
 
-const PORT = process.env.PORT || 5000;
+// ==== delete file ====
+// app.post("/delete-video", (req, res) => {
+//   let videoPath = path.join(__dirname, "/public/video/" + req.body.video);
+//   if (fs.existsSync(videoPath)) {
+//     fs.unlinkSync(videoPath);
+//     ``;
+//   }
+//   res.send("Video Deleted!");
+// });
+
+// app.post("/delete-image", (req, res) => {
+//   let imagePath = path.join(__dirname, "/public/image/" + req.body.image);
+//   if (fs.existsSync(imagePath)) {
+//     fs.unlinkSync(imagePath);
+//     ``;
+//   }
+//   res.send("Image Deleted!");
+// });
+
+const PORT = 3000;
 app.listen(PORT, console.log("Server PORT:", PORT));
